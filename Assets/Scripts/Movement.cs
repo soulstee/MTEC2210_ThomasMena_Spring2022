2fbbad21b61bdd76b5d4318c69ae4a3302bff325
Thomas Garcia Mena <thomasmena007@hotmail.com>
2022-03-23 01:43:55 -0400

    added "Frogger_Car practice" project.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections. Generic;
+using UnityEngine;
+
+
+public class Movement : MonoBehaviour
+{
+ 
+    public float speed = 5.0f;
+ 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMove = Input.GetAxisRaw("Horizontal"); 
+        float yMove = Input.GetAxisRaw("Vertical");
+
+        float xMovement = xMove * speed * Time.deltaTime;
+        float yMovement = yMove * speed * Time.deltaTime;
+
+        transform.position += Vector3.right * Time.deltaTime;
+    }
+}
